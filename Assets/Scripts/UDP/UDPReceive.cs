dd7762f81e94d8bbc19b28df03a3fff84bc1f69b
adkaros <adkaros@yahoo.com>
2017-08-30 15:37:33 -0500

    Project commit.

---
+++ Assets/Scripts/UDP/UDPReceive.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace UDP
+{
+    public class UDPReceive : SingletonBehaviour<UDPReceive>
+    {
+        public delegate void DDataReceived();
+        public event DDataReceived DataReceived;
+
+        public int port = 5000;
+
+        private Thread receiveThread;
+        private UdpClient client;
+
+        private bool send = false;
+        private bool threadOpen = true;
+
+        private string currentPacket = "";
+        private string previousPacket = "";
+
+        public void Awake()
+        {
+            Application.runInBackground = true;
+        }
+
+        public void Start()
+        {
+            port = 5000;
+            Debug.Log("Port : " + port);
+
+            receiveThread = new Thread(new ThreadStart(ReceiveData));
+            receiveThread.IsBackground = true;
+            receiveThread.Start();
+        }
+
+        // receive thread
+        private void ReceiveData()
+        {
+            client = new UdpClient(port);
+            while (threadOpen)
+            {
+                try
+                {
+                    IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, port);
+                    byte[] data = client.Receive(ref anyIP);
+
+                    // call receve callback
+                    send = true;
+                }
+                catch (Exception e)
+                {
+                    print(e.ToString());
+                }
+            }
+        }
+
+        void Update()
+        {
+            if (send)
+            {
+                if (DataReceived != null)
+                    DataReceived();
+            }
+        }
+
+        void OnApplicationQuit()
+        {
+            threadOpen = false;
+            if (receiveThread != null)
+                receiveThread.Abort();
+
+            if (client != null)
+                client.Close();
+        }
+
+        public void Reset()
+        {
+            threadOpen = false;
+            if (receiveThread != null)
+                receiveThread.Abort();
+
+            if (client != null)
+                client.Close();
+        }
+    }
+}
---
+++ Assets/Scripts/UDP/UDPSend.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace UDP
+{
+    public class UDPSend : SingletonBehaviour<UDPSend>
+    {
+        // 10.1.10.255 // All
+        // 127.0.0.1   // Self
+        public string ModelViewerIP = "10.1.10.25";
+        public int ModelViewerPort = 5000;
+
+        private IPEndPoint ModelViewerRemoteEndPoint;
+        private UdpClient ModelViewerClient;
+
+        public void Start()
+        {
+            ModelViewerRemoteEndPoint = new IPEndPoint(IPAddress.Parse(ModelViewerIP), ModelViewerPort);
+            ModelViewerClient = new UdpClient();
+        }
+
+        public void SendString(string _message)
+        {
+            try
+            {
+                if (_message != "")
+                {
+                    byte[] data = Encoding.UTF8.GetBytes(_message);
+                    ModelViewerClient.Send(data, data.Length, ModelViewerRemoteEndPoint);
+                }
+            }
+            catch (Exception err)
+            {
+                print(err.ToString());
+            }
+        }
+    }
+}
